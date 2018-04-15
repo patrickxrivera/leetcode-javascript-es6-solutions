b3f66d5fbe55f32afd7eff6c216292d6ed4b8c40
Patrick Rivera <pxr13@my.fsu.edu>
2018-04-15 19:45:45 -0400

    Added solution

---
+++ solutions/easy/binaryTreeLevelOrderTraversal.js
@@ -0,0 +1,44 @@
+// Definition for a binary tree node.
+
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+
+const solution = {
+  arr: [],
+
+  levelOrder(root) {
+    this._search(root, 1);
+    return this.arr;
+  },
+
+  _search(root, level) {
+    if (root) {
+      if (this.arr.length < level) {
+        this.arr.push([]);
+      }
+
+      const arrLevel = this.arr[level - 1];
+      arrLevel.push(root.val);
+
+      this._search(root.left, level + 1);
+      this._search(root.right, level + 1);
+      return;
+    }
+    return;
+  }
+};
+
+const tree = new TreeNode(3);
+tree.left = new TreeNode(9);
+tree.right = new TreeNode(20);
+tree.right.left = new TreeNode(15);
+tree.right.right = new TreeNode(7);
+
+solution.levelOrder(tree);
