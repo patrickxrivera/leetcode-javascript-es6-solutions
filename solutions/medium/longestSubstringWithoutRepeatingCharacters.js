0a699227076a77f7816cb7332310166385b43da5
Patrick Rivera <pxr13@my.fsu.edu>
2018-04-18 19:23:31 -0400

    Added solution

---
+++ solutions/medium/longestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  if (s.length === 0) return 0;
+
+  let str = '';
+  let max = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const idx = str.indexOf(s[i]);
+
+    if (idx !== -1) {
+      str = str.substr(idx + 1);
+    }
+
+    str += s[i];
+
+    max = Math.max(max, str.length);
+  }
+
+  return max;
+};
