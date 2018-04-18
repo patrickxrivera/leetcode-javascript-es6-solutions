233bc2c74de3c76ad1bff76f09509ff100b4f2b5
Patrick Rivera <pxr13@my.fsu.edu>
2018-04-18 19:22:45 -0400

    Added solution

---
+++ solutions/medium/threeSum.js
@@ -0,0 +1,32 @@
+var threeSum = function(nums) {
+  nums.sort(function(a, b) {
+    return a - b;
+  });
+  var target,
+    result = [],
+    len = nums.length;
+  for (var i = 0; i < len; i++) {
+    if (target === nums[i]) {
+      continue;
+    } else {
+      target = nums[i];
+    }
+    var low = i + 1,
+      high = len - 1;
+    while (low < high) {
+      if (nums[low] + nums[high] === -target) {
+        result.push([target, nums[low], nums[high]]);
+        do {
+          low++;
+        } while (nums[low] === nums[low - 1]);
+      } else if (nums[low] + nums[high] < -target) {
+        low++;
+      } else {
+        high--;
+      }
+    }
+  }
+  return result;
+};
+
+threeSum([-1, 0, 1, 2, -1, -4]);
