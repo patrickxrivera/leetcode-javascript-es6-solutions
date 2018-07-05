28e17f039af48df51f54f987d55ccb2d726afb81
Patrick Rivera <pxr13@my.fsu.edu>
2018-07-05 13:59:09 -0700

    Added solution

--- solutions/convertSortedArrayToBST.js
+++ solutions/convertSortedArrayToBST.js
@@ -1,32 +1,27 @@
-// Definition for a binary tree node.
-
-function TreeNode(val) {
-  this.val = val;
-  this.left = this.right = null;
-}
-
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
 /**
  * @param {number[]} nums
  * @return {TreeNode}
  */
 
-const getRightHalf = (nums, midpoint) => nums.slice(midpoint + 1);
-
-const createRightHalf = (nums, midpoint) => sortedArrayToBST(getRightHalf(nums, midpoint));
-
-const getLeftHalf = (nums, midpoint) => nums.slice(0, midpoint);
-
-const createLeftHalf = (nums, midpoint) => sortedArrayToBST(getLeftHalf(nums, midpoint));
-
 const sortedArrayToBST = (nums) => {
   if (nums.length === 0) return null;
-  if (nums.length === 1) return new TreeNode(nums[0]);
 
   const midpoint = Math.floor(nums.length / 2);
-  const node = new TreeNode(nums[midpoint]);
 
-  node.left = createLeftHalf(nums, midpoint);
-  node.right = createRightHalf(nums, midpoint);
+  const root = new TreeNode(nums[midpoint]);
 
-  return node;
+  const leftSide = nums.slice(0, midpoint);
+  const rightSide = nums.slice(midpoint + 1);
+
+  root.left = sortedArrayToBST(leftSide);
+  root.right = sortedArrayToBST(rightSide);
+
+  return root;
 };
