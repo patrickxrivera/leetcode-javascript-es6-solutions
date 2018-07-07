b8d6a1e94e4ac6809d0210aa405a38db283016ce
Patrick Rivera <pxr13@my.fsu.edu>
2018-07-07 12:55:20 -0700

    Added solution

--- solutions/climbStairs.js
+++ solutions/climbStairs.js
@@ -3,17 +3,31 @@
  * @return {number}
  */
 
-var climbStairs = function(n) {
+// Iteratively
+
+const climbStairs = (n) => {
   if (n <= 3) return n;
 
-  let first = 2;
-  let second = 3;
+  let curr = 3;
+  let prev = 2;
 
-  for (let i = 4; i <= n; i++) {
-    const temp = second;
-    second += first;
-    first = temp;
+  for (let i = 4; i < n + 1; i++) {
+    const temp = curr;
+    curr += prev;
+    prev = temp;
   }
 
-  return second;
+  return curr;
+};
+
+// Recursively
+
+const climbStairs = (n, memo = {}) => {
+  if (memo[n]) return memo[n];
+
+  if (n <= 2) return n;
+
+  memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+
+  return memo[n];
 };
