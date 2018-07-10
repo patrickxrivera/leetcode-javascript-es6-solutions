477e6bc2947190d0d650642c7a128d58a2630b59
Patrick Rivera <pxr13@my.fsu.edu>
2018-07-10 10:34:49 -0700

    Added solution

--- solutions/threeSum.js
+++ solutions/threeSum.js
@@ -1,32 +1,74 @@
+// var threeSum = function(nums) {
+//   nums.sort(function(a, b) {
+//     return a - b;
+//   });
+//   var target,
+//     result = [],
+//     len = nums.length;
+//   for (var i = 0; i < len; i++) {
+//     if (target === nums[i]) {
+//       continue;
+//     } else {
+//       target = nums[i];
+//     }
+//     var low = i + 1,
+//       high = len - 1;
+//     while (low < high) {
+//       if (nums[low] + nums[high] === -target) {
+//         result.push([target, nums[low], nums[high]]);
+//         do {
+//           low++;
+//         } while (nums[low] === nums[low - 1]);
+//       } else if (nums[low] + nums[high] < -target) {
+//         low++;
+//       } else {
+//         high--;
+//       }
+//     }
+//   }
+//   return result;
+// };
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
 var threeSum = function(nums) {
-  nums.sort(function(a, b) {
-    return a - b;
-  });
-  var target,
-    result = [],
-    len = nums.length;
-  for (var i = 0; i < len; i++) {
-    if (target === nums[i]) {
-      continue;
-    } else {
-      target = nums[i];
-    }
-    var low = i + 1,
-      high = len - 1;
-    while (low < high) {
-      if (nums[low] + nums[high] === -target) {
-        result.push([target, nums[low], nums[high]]);
-        do {
-          low++;
-        } while (nums[low] === nums[low - 1]);
-      } else if (nums[low] + nums[high] < -target) {
-        low++;
-      } else {
-        high--;
+  const results = [];
+  const sortedNums = nums.sort((a, b) => a - b);
+  let target = null;
+
+  for (let i = 0; i < sortedNums.length - 2; i++) {
+    if (target === nums[i]) continue;
+
+    target = nums[i];
+
+    let front = i + 1;
+    back = sortedNums.length - 1;
+
+    while (front < back) {
+      const sum = sortedNums[front] + sortedNums[back] - target;
+
+      switch (true) {
+        case sum > 0:
+          back--;
+          break;
+        case sum < 0:
+          front++;
+          break;
+        default:
+          results.push([target, sortedNums[front], sortedNums[back]]);
+
+          do {
+            front++;
+          } while (sortedNums[front] === sortedNums[front - 1]);
       }
     }
   }
-  return result;
+
+  return results;
 };
+
+threeSum([-1, 0, 1, 2, -1, -4]);
 
 threeSum([-1, 0, 1, 2, -1, -4]);
