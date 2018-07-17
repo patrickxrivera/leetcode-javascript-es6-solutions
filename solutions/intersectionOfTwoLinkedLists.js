1255e1b92887b702a417d9542d18f07ed9ab7b47
Patrick Rivera <pxr13@my.fsu.edu>
2018-07-17 10:46:27 -0700

    Added solution

---
+++ solutions/intersectionOfTwoLinkedLists.js
@@ -0,0 +1,24 @@
+// Definition for singly-linked list.
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+
+const getIntersectionNode = (headA, headB) => {
+  // get length of a
+  // get length of b
+  // get difference
+  // start longer list at length a - length b
+  // while ptrA and ptrB are not null
+  // if lists are equal
+  // return currPtr
+  // else
+  // increment node
+  // return null
+};
