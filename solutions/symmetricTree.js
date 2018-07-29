3bacf552abf26be407e729412abffe8dbbb94cef
Patrick Rivera <pxr13@my.fsu.edu>
2018-07-29 09:45:00 -0700

    Added solutions

--- solutions/symmetricTree.js
+++ solutions/symmetricTree.js
@@ -10,17 +10,9 @@ function TreeNode(val) {
  * @return {boolean}
  */
 
-const isMirror = (t1, t2) => {
+const isSymmetric = (t1, t2 = t1) => {
   if (t1 === null && t2 === null) return true;
   if (t1 === null || t2 === null) return false;
 
-  return (
-    t1.val === t2.val &&
-    isMirror(t1.right, t2.left) &&
-    isMirror(t1.left, t2.right)
-  );
-};
-
-var isSymmetric = function(root) {
-  return isMirror(root, root);
+  return t1.val === t2.val && isSymmetric(t1.right, t2.left) && isSymmetric(t1.left, t2.right);
 };
